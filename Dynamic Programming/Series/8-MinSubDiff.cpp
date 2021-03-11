44b2bc4ba74fef8ae5e6cb0987b19fcdc01ee9e3
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-03-11 22:16:57 +0530

    dp

--- Dynamic Programming/Series/8-MinSubDiff.cpp
+++ Dynamic Programming/Series/8-MinSubDiff.cpp
@@ -22,16 +22,28 @@ int summing(vector<int>& arr, int n, int sum) {
   return dp[n-1][sum] = summing(arr, n-1, sum-arr[n-1]) || summing(arr, n-1, sum);
 }
 
-int main(){
+int MinSubDiff(vector<int> &arr, int n) {
+  ll sum=0;
+  for(int i: arr) sum += i;
 
+  bool S[sum/2+1] = {false};
+  for(int i=0; i<sum/2+1; i++) 
+    if(summing(arr, n, i)) S[i] = true;
+
+  ll mn=INT_MAX;
+  for(int i=sum/2; i>=0; i--) 
+    if(S[i]) {mn = min(mn, sum-2*i);}
+
+  return mn;  
+}
+
+int main(){
   ios :: sync_with_stdio(false);
   cin.tie(0);
 
   memset(dp, -1, sizeof(dp));
+  vector<int> arr = {3, 1, 4, 2, 2, 1};
 
-  vector<int> arr = {40, 10, 50 ,120};
-  int W = 50;
-
-  cout << summing(arr, 4, W);
+  cout << MinSubDiff(arr, 6);
   return 0;
 }
---
+++ Dynamic Programming/Series/9-SubsetWithDiff.cpp
@@ -0,0 +1,50 @@
+/*!
+* Copyright (c) 2020 Abhishek Srivastava
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int dp[1001][1001];
+
+bool SubsetSum(vector<int>& arr, int n, int sum) {
+  if(sum == 0) return true;
+  if(n == 0) return false;
+
+  if(dp[n-1][sum] != -1) return dp[n-1][sum];
+  if(arr[n-1] > sum) return dp[n-1][sum] = SubsetSum(arr, n-1, sum);
+
+  return dp[n-1][sum] = SubsetSum(arr, n-1, sum-arr[n-1]) || SubsetSum(arr, n-1, sum);
+}
+
+int SubsetWithGivenDiff(vector<int>& arr, int n, int diff) {
+  ll sum=0;
+  for(auto i: arr) sum += i;
+  bool S[sum]={false};
+  
+  for(int i=0; i<sum; i++) 
+    if(SubsetSum(arr, n, i)) S[i] = true;
+
+  // for(auto i: S) cout << i << "\n";
+
+  int ans=0;
+  for(int i=0; i<sum-diff; i++)
+    if(S[i] && S[i+diff]) ans++;
+  
+  return ans;
+}
+
+int main(){
+
+  ios :: sync_with_stdio(false);
+  cin.tie(0);
+
+  memset(dp, -1, sizeof(dp));
+  vector<int> arr = {1, 2, 7};
+  int diff = 4;
+
+  cout << SubsetWithGivenDiff(arr, 3, diff);
+ 
+  return 0;
+}
