626f561708cb0b71f85451a319770a8f0de7ecd7
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-03-14 21:58:29 +0530

    DP

--- Dynamic Programming/Series/BOUNDED KNAPSACK/4-coins-change2.cpp
+++ Dynamic Programming/Series/BOUNDED KNAPSACK/4-coins-change2.cpp
@@ -17,8 +17,8 @@ int change2(vector<int> c, int n, int W) {
   }
 
   if(dp[n-1][W] != -1) return dp[n-1][W];
-  if(c[n-1] > W) return change(c, n-1, W);
-  return min(change(c, n, W-c[n-1]), change(c, n-1, W));
+  if(c[n-1] > W) return change2(c, n-1, W);
+  return min(change2(c, n, W-c[n-1]), change2(c, n-1, W));
 }
 
 
@@ -28,8 +28,21 @@ int change(vector<int> c, int n, int sum) {
   for(int i=0; i<n; i++) dp[i][0] = 0;
 
   for(int i=1; i<sum+1; i++) 
-    if(j%c[0] == 0) dp[1][j] = j/c[0];
-    else dp[1][j] = INT_MAX-1;
+    if(i%c[0] == 0) dp[1][i] = i/c[0];
+    else dp[1][i] = INT_MAX-1;
+
+  for(int i=1; i<n+1; i++) {
+    for(int j=2; j<sum+1; j++) {
+      if(c[i-1] <= j) {
+        dp[i][j] = min(dp[i][j-c[i-1]]+1, dp[i-1][j]);
+      }
+      else {
+        dp[i][j] = dp[i-1][j];
+      }
+    }
+  }
+
+  return dp[n][sum];
 
 }
 
@@ -40,8 +53,8 @@ int main(){
   cin.tie(0);
 
   memset(dp, -1, sizeof(dp));
-  vector<int> coins = {1, 2, 3};
-  int n=3, W=5;
+  vector<int> coins = {9, 6, 5, 1};
+  int n=4, W=11;
 
   cout << change(coins, n, W) << "\n";
 
