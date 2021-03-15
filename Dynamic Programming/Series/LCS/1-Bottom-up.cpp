5054c35fca1517e2902780e04622af348a95fa36
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-03-16 04:40:16 +0530

    LCS

--- Dynamic Programming/Series/LCS/1-Bottom-up.cpp
+++ Dynamic Programming/Series/LCS/1-Bottom-up.cpp
@@ -6,8 +6,13 @@
 using namespace std;
 #define ll long long int
 
-void LCS() {
+int dp[1001][1001];
 
+int LCS(string X, string Y, int n, int m) {
+  if(n == 0 || m == 0) return 0;
+  if(dp[n][n] != -1) return dp[n][m];
+  if(X[n-1] == Y[m-1]) return dp[n][m] = 1 + LCS(X, Y, n-1, m-1);
+  else return dp[n][m] = max(LCS(X, Y, n-1, m), LCS(X, Y, n, m-1));
 }
 
 int main(){
@@ -15,6 +20,17 @@ int main(){
   ios :: sync_with_stdio(false);
   cin.tie(0);
 
+  memset(dp, -1, sizeof(dp));
 
+
+  string X = "abhidfa";
+  string Y = "abidpkkka";
+  // answer = "abida", i.e, 5
+
+  int n = X.length();
+  int m = Y.length();
+  
+  cout << LCS(X, Y, n, m);
+  
   return 0;
 }
--- Dynamic Programming/Series/LCS/1-Top-Down.cpp
+++ Dynamic Programming/Series/LCS/1-Top-Down.cpp
@@ -6,15 +6,36 @@
 using namespace std;
 #define ll long long int
 
-void LCS() {
-
-}
 
 int main(){
 
   ios :: sync_with_stdio(false);
   cin.tie(0);
 
+  int dp[1001][1001];
+  memset(dp, -1, sizeof(dp));
 
+  string X = "abhidfa";
+  string Y = "abidpkkka";
+  // answer = "abida", i.e, 5
+
+  int n = X.length();
+  int m = Y.length();
+
+  // loops
+  
+  for(int i=0; i<n+1; i++)
+    for(int j=0; j<m+1; j++) {    
+      // Initialization  
+      if(i == 0 || j == 0) 
+        dp[i][j] = 0;
+      // calls
+      else if(X[i-1] == Y[j-1])
+        dp[i][j] = 1 + dp[i-1][j-1];
+      else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+    }
+  
+  cout << dp[n][m];
+  
   return 0;
 }
