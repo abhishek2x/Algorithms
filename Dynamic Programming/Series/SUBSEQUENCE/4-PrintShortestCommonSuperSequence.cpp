9ef87bb00c84835f58e5fbc00164c05c8bd35601
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-07-09 21:48:46 +0530

    correction

--- Dynamic Programming/Series/SUBSEQUENCE/4-PrintShortestCommonSuperSequence.cpp
+++ Dynamic Programming/Series/SUBSEQUENCE/4-PrintShortestCommonSuperSequence.cpp
@@ -17,8 +17,8 @@ int main(){
   int dp[1001][1001];
   memset(dp, -1, sizeof(dp));
 
-  string X = "AGGTAB";
-  string Y = "GXTXAYB";
+  string X = "ABCD";
+  string Y = "ABPQD";
   // answer = "AGXGTXAYB"
 
   int m = X.length();
@@ -42,11 +42,11 @@ int main(){
       i--; j--;
     }
     else if(dp[i-1][j] > dp[i][j-1]) {
-      ans += Y[j-1];
-      j--;
-    } else {
       ans += X[i-1];
       i--;
+    } else {
+      ans += Y[j-1];
+      j--;
     }
   }
 
@@ -59,7 +59,7 @@ int main(){
     j--;
   }
  
-  // reverse(ans.begin(), ans.end());
+  reverse(ans.begin(), ans.end());
 
   cout << ans;
 
