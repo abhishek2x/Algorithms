dfbf9ee600f3ed0e44806ecc1cabb411e7ab5814
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-07-10 11:09:06 +0530

    rename + edits

--- Dynamic Programming/Series/SUBSEQUENCE/4-PrintShortestCommonSuperSequence.cpp
+++ Dynamic Programming/Series/SUBSEQUENCE/4-PrintShortestCommonSuperSequence.cpp
@@ -2,9 +2,6 @@
 * Copyright (c) 2020 Abhishek Srivastava
 */
 
-
-// revisit: https://www.youtube.com/watch?v=VDhRg-ZJTuc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=29
-
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
@@ -17,51 +14,48 @@ int main(){
   int dp[1001][1001];
   memset(dp, -1, sizeof(dp));
 
-  string X = "ABCD";
-  string Y = "ABPQD";
-  // answer = "AGXGTXAYB"
+  string X = "abcd";
+  string Y = "abpqd";
 
-  int m = X.length();
-  int n = Y.length();
+  int m=X.length();
+  int n=Y.length();
 
   int i, j;
-
-  for(i=0; i<=m; i++) 
-    for(j=0; j<=n; j++) {
-      if(i == 0 || j == 0) dp[i][j] = 0;
-      else if(X[i-1] == Y[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+  
+  for(int i=0; i<m+1; i++) 
+    for(int j=0; j<n+1; j++) {
+      if(i==0 || j == 0) dp[i][j] = 0;
+      else if(X[i-1] == Y[j-1]) dp[i][j] = 1+dp[i-1][j-1];
       else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
     }
 
-
-  i = m; j = n;
+  i=m; j=n;
   string ans = "";
-  while(i>0 && j>0) {
+  while(i > 0 && j > 0) {
     if(X[i-1] == Y[j-1]) {
-      ans += X[i-1];
+      ans += X[i-1]; 
       i--; j--;
     }
     else if(dp[i-1][j] > dp[i][j-1]) {
       ans += X[i-1];
       i--;
-    } else {
+    }
+    else {
       ans += Y[j-1];
       j--;
-    }
+    } 
   }
 
-  while (i > 0) { 
-    ans += X[i-1];
-    i--;
-  } 
-  while (j > 0) { 
-    ans += Y[j-1];
-    j--;
+  while(i>0) {
+    ans += X[i]; i--;
   }
- 
+
+  while(j>0) {
+    ans += Y[j]; j--;
+  }
+
   reverse(ans.begin(), ans.end());
-
   cout << ans;
-
+  
   return 0;
 }
