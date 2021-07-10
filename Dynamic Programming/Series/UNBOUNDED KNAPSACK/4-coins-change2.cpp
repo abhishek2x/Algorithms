ef51467d98ebec300c0411237ff719768ad86ba3
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-07-10 12:49:59 +0530

    initialisation of changes

--- Dynamic Programming/Series/UNBOUNDED KNAPSACK/4-coins-change2.cpp
+++ Dynamic Programming/Series/UNBOUNDED KNAPSACK/4-coins-change2.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int dp[1001][1001];
 
+// Bottom up
 int change2(vector<int> c, int n, int W) {
   if(n == 0) return INT_MAX-1;
   if(W == 0) return 0;
@@ -21,29 +22,40 @@ int change2(vector<int> c, int n, int W) {
   return min(change2(c, n, W-c[n-1]), change2(c, n-1, W));
 }
 
-
+// Top down
 int change(vector<int> c, int n, int sum) {
 
   for(int i=0; i<sum+1; i++) dp[0][i] = INT_MAX-1;
-  for(int i=0; i<n; i++) dp[i][0] = 0;
+  for(int i=1; i<n; i++) dp[i][0] = 0;
 
-  for(int i=1; i<sum+1; i++) 
-    if(i%c[0] == 0) dp[1][i] = i/c[0];
-    else dp[1][i] = INT_MAX-1;
+  for(int j=1; j<sum+1; j++) 
+    if(j%c[0] == 0) dp[1][j] = j/c[0];
+    else dp[1][j] = INT_MAX-1;
 
   for(int i=1; i<n+1; i++) {
     for(int j=2; j<sum+1; j++) {
       if(c[i-1] <= j) {
-        dp[i][j] = min(dp[i][j-c[i-1]]+1, dp[i-1][j]);
+        dp[i][j] = min(dp[i][j-c[i-1]] + 1, dp[i-1][j]);
       }
       else {
         dp[i][j] = dp[i-1][j];
       }
     }
   }
-
   return dp[n][sum];
+}
 
+// CO-PILOT
+int coins(vector<int> c, int n, int sum) {
+  int dp[sum+1];
+  for(int i=0; i<=sum; i++) dp[i] = INT_MAX-1;
+  dp[0] = 0;
+  for(int i=1; i<=sum; i++) {
+    for(int j=0; j<n; j++) {
+      if(c[j] <= i) dp[i] = min(dp[i], dp[i-c[j]]+1);
+    }
+  }
+  return dp[sum];
 }
 
 
