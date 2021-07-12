47fccb57812496b823b13cc1ddfe2aac97eb17db
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-07-12 21:53:15 +0530

    Automated commit msg

--- Graph/3-Cycle-Undirected-BFS.cpp
+++ Graph/3-Cycle-Undirected-BFS.cpp
@@ -3,3 +3,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isCycleBFS(int node, vector<int> adj[], vector<int> &visited) {
+    queue<int> q;
+    q.push(node);
+    visited[node] = true;
+
+    while(!q.empty()) {
+        int temp = q.front();
+        q.pop();
+        
+        for(auto it: adj[temp]) {
+            if(!visited[it]) {
+                q.push(it);
+                visited[it] = 1;
+            }
+            else if(temp != it) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool checkCycle(int V, vector<int> adj[]) {
+    vector<int> visited(V+1, 0);
+    
+    for(int i=0; i<=V; i++) {
+        if(!visited[i])
+            if(isCycleBFS(i, adj, visited)) 
+                return true;
+    }
+    return false;
+}
--- Graph/4-Cycle-Undirected-DFS.cpp
+++ Graph/4-Cycle-Undirected-DFS.cpp
@@ -1,58 +1,28 @@
-// Checking cycle in undirected graph using DFS
+// Cycle in undirected using DFS
 
 #include<bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
+bool isCycleDFS(int src, vector<int> adj[], vector<int>& visited) {
+	visited[src] = 1;
 
-
-class Solution {
-
-public:
-    bool checkForCycle(int node, int parent, vector<int>& visited, vector<int> adj[]) {
-        
-        visited[node] = true;
-        for(auto it: adj[node]) {
-            if(!visited[it]) {
-                if(checkForCycle(it, node, visited, adj)) return true;
-            }
-            else if(it != parent) return true;
-        }
-        return false;
-    }
-
-public:
-	bool isCycle(int V, vector<int>adj[]){
-        vector<int> visited(V, 0);
-
-        // Loop is used to check cycle in each component
-        for(int i=0; i<V; i++) {
-            if(!visited[i]) 
-                if(checkForCycle(i, -1, visited, adj)) return true;
-        }
-        return false;
-	}
-};
-
-// { Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int V, E;
-		cin >> V >> E;
-		vector<int>adj[V];
-		for(int i = 0; i < E; i++){
-			int u, v;
-			cin >> u >> v;
-			adj[u].push_back(v);
-			adj[v].push_back(u);
+	for(auto it: adj[src]) {
+		if(!visited) {
+			if(isCycleDFS(it, adj, visited)) return true;
 		}
-		Solution obj;
-		bool ans = obj.isCycle(V, adj);
-		if(ans)
-			cout << "1\n";
-		else cout << "0\n";
+		else if(it != src) return true;
 	}
-	return 0;
-}  // } Driver Code Ends
+	return false;
+}
+
+
+bool checkCycle(int V, vector<int> adj[]) {
+    vector<int> visited(V+1, 0);
+    
+    for(int i=0; i<=V; i++) {
+        if(!visited[i])
+            if(isCycleDFS(i, adj, visited)) 
+                return true;
+    }
+    return false;
+}
