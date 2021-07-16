4a772464c74e529884138807349e2470c027d29b
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-07-16 20:48:56 +0530

    Automated commit msg

--- Graph/14-Prim.cpp
+++ Graph/14-Prim.cpp
@@ -1,2 +1,80 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+
+int N;
+
+// <node, weight>
+vector<pair<int, int>> adj[];
+
+vector<int> parent(N, -1);
+vector<int> key(N, INT_MAX);
+vector<bool> mst(N, false);
+
+// O(NlogN)
+void prim_Optimised(int src) {
+    
+    // feat: priority queue -> Min Heap -> Min on top
+    priority_queue< pair<int, int>,
+                    vector<pair<int, int>>,
+                    greater<pair<int, int>> > pq;
+
+    key[0] = 0;
+    parent[0] = -1;
+    pq.push({0, 0});
+
+    for(int i=0; i<N-1; i++) {
+
+        // finding min in key array
+        int u = pq.top().second;
+        pq.pop();
+
+        mst[u] = true;
+
+        for(auto it: adj[u]) {
+            int weight = it.second;
+            int node = it.first;
+
+            if(!mst[node] && key[node] > weight) {
+                key[node] = weight;
+                parent[node] = u;
+                // push in priority queue
+                pq.push(make_pair(key[node], node));
+            }
+        }
+    }
+
+    // print parent array
+}
+
+// O(N^2)
+void prim_bruteforce(int src) {
+    parent[0] = -1;
+    key[0] = 0;
+
+    for(int count=0; count<N-1; count++){
+        
+        // find min key
+        int mini = INT_MAX, u=0;
+        for(int i=0; i<N; i++) {
+            if(!mst[i] && key[i] < mini) {
+                mini = key[i];
+                u = i;
+            }
+        }
+
+        mst[u] = true;
+
+        // do work
+        for(auto neigh: adj[u]) {
+            int weight = neigh.second;
+            int node = neigh.first;
+
+            if(!mst[node] && key[node] > weight) {
+                parent[node] = u;
+                key[node] = weight;
+            }
+        }
+    }
+
+    // parent is answer
+}
--- Graph/15-Disjoint.cpp
+++ Graph/15-Disjoint.cpp
@@ -1,2 +1,43 @@
+// It's a Data Structure which maintains common parent
+// of all node belonging to the same component.
+
+// Application: Kruskal's Algorithm
+
+
 #include <bits/stdc++.h>
 using namespace std;
+
+int parentArr[100000];
+int rankArr[100000];
+
+void makeSet() {
+    for(int i=0; i<100000; i++) {
+        parentArr[i] = i;
+        rankArr[i] = 0;
+    }
+}
+
+int findPar(int src) {
+    if(src == parentArr[src]) return src;
+    else parentArr[src] = findPar(parentArr[src]);
+}
+
+void unionn(int u, int v) {
+    u = findPar(u);
+    v = findPar(v);
+
+    if(rankArr[u] < rankArr[v]) 
+        parentArr[u] = v;
+    else if(rankArr[v] < rankArr[u]) 
+        parentArr[v] = u;
+    else {
+        parentArr[u] = v;
+        rankArr[u]++;
+    }
+}
+
+int main() {
+    // Take input for values of u and v;
+
+    // if Parent of u and v are same -> same component
+}
