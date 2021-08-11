11a5ccbb2381654cf608a755edce8f0c1c121326
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-08-11 15:27:55 +0530

    Automated commit msg

---
+++ Bit Manipulation/1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void solve() {
+    vector<int> v;
+
+    for(int i=0; i<10; i++) {
+        for(int j=0; j<10; j++) {
+            cout << "BITCH!!";
+        }
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    while(t--)
+        solve();
+
+    return 0;
+}
---
+++ Bit Manipulation/README.md
@@ -0,0 +1,8 @@
+# Tips and Tricks
+
+
+## BASIC
+- a << 1 -> Multiply the number by 2
+- a << b -> Multiply the number by 2^b
+- a >> 1 -> Divide the number by 2
+- a >> b -> Divide the number by 2^b
